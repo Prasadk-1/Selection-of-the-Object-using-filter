c9cd7740041f9709bef7e979b10aa7163594633d
Prasadk-1 <87906300+Prasadk-1@users.noreply.github.com>
2021-08-01 10:37:22 +0530

    Add files via upload
    Selection of the Object using Filter

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("Hello PK");
+
+const students = [
+    {name: "Adhi", gender:"male"},
+    {name: "Ahmed", gender:"male"},
+    {name:"Rutika Kadam", gender:"female"},
+    {name:"Priyadarshani", gender:"female"}
+    ];
+    
+    let femaleName = [];
+    let nameInitialA =[];
+    
+    let Females = students.filter(function(gen){
+        if (gen.gender==="female"){
+            femaleName.push(gen.name);
+        }
+    });
+   
+    let nameStartsWithA = students.filter(function(initial){
+      if(initial.name.charAt(0)==="A"){
+          nameInitialA.push(initial.name);
+      }
+    });
+   
+    let noOfMales =students.filter(function(gen){
+      return gen.gender==="male";
+    });
+
+  
+     console.log(femaleName);
+     console.log(nameInitialA);
+     console.log(noOfMales.length);
